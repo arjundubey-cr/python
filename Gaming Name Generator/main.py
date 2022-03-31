be4c499936bb03acc97d3bae8dcc8341f42537a5
Arjun Dubey <arjundubeycr@gmail.com>
2022-03-31 11:02:09 +0530

    gaming name generator

---
+++ Gaming Name Generator/main.py
@@ -0,0 +1,7 @@
+print("Enter the city you grewup in:")
+city = input()
+
+print("Enter your pet's name:")
+petName = input()
+
+print("The gaming name for you can be: " + " " + city + " " + petName)
